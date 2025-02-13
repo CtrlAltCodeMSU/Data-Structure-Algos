76163b564ab8c7a003f8b9afd8e43d1195a5a920
CtrlAltCodeMSU <muhammadsanaullah4443@gmail.com>
2025-02-13 23:16:27 +0500

    2ndLargestElement

---
+++ DSA Algorithms/ArraysOperations/Find2ndLargestElement.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int secondLargest(vector<int> &array){
+    int n = array.size();
+
+    int largest = -1 , secondLarge = -1;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (array[i]>largest)
+        {
+            largest = array[i];
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (array[i] > secondLarge && array[i] != largest)
+        {
+            secondLarge = array[i] ;
+        }
+        
+    }
+    return secondLarge;
+    
+}
+int main(){
+    vector<int> arr =  {12, 35, 1, 10, 34, 1};
+    
+    cout << "\n " <<secondLargest(arr);
+    return 0;
+}
