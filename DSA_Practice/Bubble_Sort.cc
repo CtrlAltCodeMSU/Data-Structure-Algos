d03ae933422a33a75655fab4455aa5da66b176bb
CtrlAltCodeMSU <muhammadsanaullah4443@gmail.com>
2025-02-17 11:45:44 +0500

    Bubble Sort

---
+++ DSA_Practice/Bubble_Sort.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+void bubbleSort(int array[], int size){
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0 ; j < size - i - 1 ; j++)
+        {
+            if (array[j]>array[j+1])
+            {
+                int temp = array[j] ;
+                array[j] = array[j+1];
+                array[j+1] = temp ;
+            }
+        }
+    }
+}
+void printArray(int array[] , int size){
+    for (int i = 0; i < size ; i++)
+    {
+        cout << array[i] << " " ;
+    }
+    cout << endl ;
+}
+int main(){
+    int size ;
+    cout << "Enter the size for Array: " ;
+    cin >> size ;
+    
+    int array[size];
+
+    cout << "Enter " << size << " Elements: " ;
+    for (int i = 0; i < size ; i++)
+    {
+        cin >> array[i] ;
+
+    }
+    
+
+    cout << "Original Array: " ;
+    printArray(array,size);
+
+    bubbleSort(array , size);
+
+    cout << "Sorted Array: " ;
+    printArray(array,size);
+    return 0;
+}
