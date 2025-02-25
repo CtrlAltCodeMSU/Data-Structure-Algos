04ca3e9b4a425fe1e615b652306887096940ee2d
CtrlAltCodeMSU <muhammadsanaullah4443@gmail.com>
2025-02-25 09:28:06 +0500

    C Plus Plus

--- DSA Lab/Lab_02/hi.cpp
+++ DSA Lab/Lab_02/hi.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 using namespace std;
+void max_min(int array[], int size){
+    int max = array[0] ;
+    int min = array[0] ;
 
-int main(){
-    int size1  ;
-    
-    cout << "Enter Size: " ;
-    cin >> size1 ;
-    int array[size1];
-    cout << "Enter "<< size1 << " Array Elements: " ;
-    for (int i = 0; i < size1 ; i++){
-        cin >> array[i] ;
+    for (int i = 0; i < size; i++){
+        if (array[i]>max){
+            max = array[i] ;
+        }
+       else if(array[i]<min){
+           min = array[i] ;
+       }
     }
+    cout << "Max Element: " << max << endl;
+    cout << "Min Element: " << min << endl;   
+}
+int main(){
+    int size = 5 ;
+    int array[size] = {10,12,85,7,5};
 
-    // sumOfArray(array , size1);
+    max_min(array , size);
+    
     return 0;
 }
