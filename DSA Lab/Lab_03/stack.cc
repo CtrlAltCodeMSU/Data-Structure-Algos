c54669b656d48dc5b71315fb823e7ab3b3f22be2
CtrlAltCodeMSU <muhammadsanaullah4443@gmail.com>
2025-03-05 19:33:27 +0500

    Modification

--- DSA Lab/Lab_03/stack.cc
+++ DSA Lab/Lab_03/stack.cc
@@ -2,25 +2,40 @@
 #include <stack>
 using namespace std;
 
-int main(){
-    stack<int> s ;
+void pop(int value, stack<int>, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+    }
+}
+int main()
+{
+    stack<int> s;
 
-    s.push(10);
-    s.push(20);
-    s.push(30);
-    s.push(40);
-    s.push(50);
-    
+    int value, size;
+    cout << "Enter Size: ";
+    cin >> size;
+    cout << "Enter " << size << " Values for push: " << endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Enter Value " << (i + 1) << " : ";
+        cin >> value;
+        s.push(value);
+    }
+
     cout << s.top() << endl;
-        
-
     while (!s.empty())
     {
-        cout << s.top() << " " ;
+        cout << s.top() << " ";
         s.pop();
     }
     cout << endl;
-    
+
     return 0;
-    
 }
+// s.push(10);
+// s.push(20);
+// s.push(30);
+// s.push(40);
+// s.push(50);
