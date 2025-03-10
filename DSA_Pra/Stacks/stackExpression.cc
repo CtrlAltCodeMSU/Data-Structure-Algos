27960ccb1aae91a235ea8d104a37a4525661e57a
CtrlAltCodeMSU <muhammadsanaullah4443@gmail.com>
2025-03-10 21:57:12 +0500

    Stack Expression

--- DSA_Pra/Stacks/stackExpression.cc
+++ DSA_Pra/Stacks/stackExpression.cc
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cmath>
+#include <stack>
+#include <sstream>
 using namespace std;
 int performOperation(int operand1 , int operand2 , char operation){
     switch (operation){
@@ -21,10 +23,40 @@ int performOperation(int operand1 , int operand2 , char operation){
         break;
     default:
         cerr << "Error: Invalid Operation " << operation << endl;
+        exit(EXIT_FAILURE);
         break;
     }
 }
+int postValueExpression(const string& expression){
+    stack<int> stack ;
+
+    for(char c: expression){
+        if(isdigit(c)){
+            stack.push(c - '0');
+        }else{
+            if(stack.size()<2){
+                cerr << "Error: Invalid postfix expression!" << endl;
+                exit(EXIT_FAILURE);
+            }
+            int operand2 = stack.top() ; stack.pop();
+            int operand1 = stack.top() ; stack.pop();
+
+            int result = performOperation(operand1 , operand2 , c);
+            stack.push(result);
+        }
+    }
+    if(stack.size()!=1){
+        cerr << "Error: Invalid postfix expression!" << endl;
+        exit(EXIT_FAILURE);
+    }
+    return stack.top();
+}
 int main(){
-    
+
+    string postfix ;
+    cout  << "Enter your Expression (+,-,*,/,^): " ;
+    getline(cin ,  postfix) ;
+
+    cout  << "Result: " << postValueExpression(postfix) << endl;
     return 0;
 }
