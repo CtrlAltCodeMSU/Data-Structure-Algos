7991822e76b8d8b121568699e77ee0a5074532b2
CtrlAltCodeMSU <muhammadsanaullah4443@gmail.com>
2025-03-11 00:49:36 +0500

    Stacks

--- DSA_Pra/Stacks/complexity.cc
+++ DSA_Pra/Stacks/complexity.cc
@@ -1,25 +1,25 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main(){
-    int i ; j ; k = 0 ;
-    for (cnt i = n/2; i <=n ;i++)
-    {
-        for ( j = 2; i <= n ; j*=2)
-        {
-            k = k + n/2 ;
-        }
+// int main(){
+//     int i ; j ; k = 0 ;
+//     for (cnt i = n/2; i <=n ;i++)
+//     {
+//         for ( j = 2; i <= n ; j*=2)
+//         {
+//             k = k + n/2 ;
+//         }
         
-    }
+//     }
 
-    int a = 0 ; i =N ;
-    while (i>0)
-    {
-        a+=i ;
-        i/=2;
-    }
+//     int a = 0 ; i =N ;
+//     while (i>0)
+//     {
+//         a+=i ;
+//         i/=2;
+//     }
     
 
     
-    return 0;
-}
+//     return 0;
+// }
