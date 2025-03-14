1fbba07cdf63286fb1e5e870039e6e1e77331848
CtrlAltCodeMSU <muhammadsanaullah4443@gmail.com>
2025-03-14 06:43:32 +0500

    Threads

--- DSA_Pra/Queues/os.cc
+++ DSA_Pra/Queues/os.cc
@@ -1,19 +1,42 @@
+// #include <iostream>
+// #include <unistd.h>  // For fork()
+
+// int main() {
+//     int x = 5; // Each process will have its own 'x'
+    
+//     pid_t pid = fork(); // Creates a new process
+
+//     for (int i = 0 ; i < 100 ; i ++){
+//     if (pid == 0) {
+//         // Child Process
+//         x = 10; 
+//         std::cout << "Child Process: x = " << x << std::endl;
+//     } else {
+//         // Parent Process
+//         std::cout << "Parent Process: x = " << x << std::endl;
+//     }
+// }
+
+//     return 0;
+// }
 #include <iostream>
-#include <unistd.h>  // For fork()
+#include <thread>
+
+void taskA() {
+    int localA = 10;  // Stored in thread A's stack
+    std::cout << "Thread A: " << localA << std::endl;
+}
+
+void taskB() {
+    int localB = 20;  // Stored in thread B's stack
+    std::cout << "Thread B: " << localB << std::endl;
+}
 
 int main() {
-    int x = 5; // Each process will have its own 'x'
-    
-    pid_t pid = fork(); // Creates a new process
+    std::thread thread1(taskA);
+    std::thread thread2(taskB);
 
-    if (pid == 0) {
-        // Child Process
-        x = 10; 
-        std::cout << "Child Process: x = " << x << std::endl;
-    } else {
-        // Parent Process
-        std::cout << "Parent Process: x = " << x << std::endl;
-    }
-
+    thread1.join();
+    thread2.join();
     return 0;
 }
