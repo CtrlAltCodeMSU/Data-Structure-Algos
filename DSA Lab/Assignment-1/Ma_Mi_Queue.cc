10cc36a9fc9ddaf13252bba8fe94317238f42ba0
CtrlAltCodeMSU <muhammadsanaullah4443@gmail.com>
2025-03-16 21:48:41 +0500

    A-1

---
+++ DSA Lab/Assignment-1/Ma_Mi_Queue.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+#define MAX_SIZE 10
+int main(){
+    queue<int> values; 
+    int num , minValue , maxValue ;
+    cout << "Enter " << MAX_SIZE << " Stack Values: " ;
+    for (int i = 0; i < MAX_SIZE; i++){
+        cin >> num ;
+        values.push(num);
+    }
+    minValue = maxValue = values.front();
+    while (!values.empty()){
+        int element = values.front();
+        values.pop();
+        if(element>maxValue){
+            maxValue = element ;
+        }
+        if(element<minValue){
+            minValue = element ;
+        }
+    }
+    cout << "Max_Value is: " << maxValue << endl;
+    cout << "Min_Value is: " << minValue << endl;
+    return 0;
+}
